aef6f6c5452b707293194536cc06e189715eb8ce
Arun Vasudevan <arunv83@gmail.com>
2016-01-30 13:00:10 -0800

    "Created Recursion Problems site files. To be hosted on gh-pages."

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# RecursionProblems
-A collection of Recursion problems using JavaScript, HTML, CSS, and JQuery.
+#Recursion Problems
+
+###by Arun Vasudevan
+
+This is a one-page website with several JavaScript problems using recursion. The site enables the visitor to test each problem with inputs and see the results.
+
+The site was built using:
+
+1. JavaScript
+2. HTML
+3. CSS
+4. JQuery
+5. Bootstrap
+
+####Usage
+
+Once you enter an input (or inputs) into the fields, press **Calculate** to ‘solve’ the problem. A dynamically generated answer will appear on the page. Press **Clear** to clear all fields and try again.
---
+++ js/recursionproblems.js
@@ -0,0 +1,389 @@
+//RecursionProblems.js
+
+        //reload page on title click
+        $('#name').click(function (e) {
+            e.preventDefault();
+            location.reload();
+        });
+
+    	//commas every 3 numbers
+	    function commaSeparateNumber(val){
+	       while (/(\d+)(\d{3})/.test(val.toString())){
+	         val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+	       }
+	       return val;
+	    };
+
+    	//No. 1 - Write a JavaScript program to calculate the factorial of a number.
+    	var factorial = (num) => (num === 0) ? 1 : (num * factorial(num - 1));
+
+        $('#fact-result').hide();
+
+    	$('#fact-button').click(function (e) {
+    		e.preventDefault();
+    		var myVal = $('#factorial-field').val();
+    		var result = commaSeparateNumber(factorial(myVal));
+    		console.log('result ' , result);
+    		var ans = $('#fact-result').append(result).hide();
+    		ans.slideDown();
+    	});
+
+    	$('#fact-clear').click(function (event) {
+    		event.preventDefault();
+    		location.reload(); //clear both fields    		
+    	});
+
+    	//No. 2 - Write a JavaScript program to find the greatest common divisor (gcd) of two positive numbers.
+    	var gcd = (num1, num2) => {
+            var result = null;
+
+            return (num2 === 0) ? result = num1 : (result = gcd(num2, num1 % num2));
+    	};
+
+        console.log(gcd(108,30));
+
+        $('#gcd-result').hide();
+
+        $('#gcd-button').click(function (e) {
+            e.preventDefault();
+            var num1 = $('#gcd-field-one').val();
+            var num2 = $('#gcd-field-two').val();
+            var result = commaSeparateNumber(gcd(num1, num2));
+            console.log('result ' , result);
+            var ans = $('#gcd-result').append(result).hide();
+            ans.slideDown();
+        });
+
+        $('#gcd-clear').click(function (event) {
+            event.preventDefault();
+            location.reload(); //clear both fields          
+        });
+
+        //No. 3 - Write a JavaScript program to get the integers in range (x, y)
+        var finArr = []; //initialize outside
+        var range = (min, max) => {
+          var result = null;
+
+          if ((max - min) === 2) {
+              return [max - 1];
+          } else {                      
+            finArr = range(min, max - 1);            
+            finArr.push(max - 1);
+            return finArr;
+          }
+        };
+
+        console.log(range(2,9));
+
+        $('#range-result').hide();
+
+        $('#range-button').click(function (e) {
+            e.preventDefault();
+            var num1 = $('#range-field-one').val();
+            var num2 = $('#range-field-two').val();
+            var result = range(num1, num2);
+            console.log('result ' , result);
+            var ans = $('#range-result').append('[' + result + ']').hide();
+            ans.slideDown();
+        });
+
+        $('#range-clear').click(function (event) {
+            event.preventDefault();
+            location.reload(); //clear both fields          
+        });
+
+        //No. 4 - Write a JavaScript program to compute the sum of an array of integers.
+        var arraySum = (array) => {
+          var result = null;          
+
+          if (array.length === 1) {
+            result = array[0];
+          } else {            
+            result = array.pop() + arraySum(array);
+          }
+          return result;
+        };
+
+        console.log(arraySum([1,2,3,4,5]));        
+
+        $('#sum-result').hide();
+
+        $('#sum-button').click(function (e) {
+            e.preventDefault();
+            
+            var elem = $('#sum-field').val();
+            console.log('elem ' , elem, 'typeof(elem)' , typeof(elem));
+
+            //create array of values
+            var numbers = [];
+            if (_.contains(elem.split(''), ',')) {
+                numbers = elem.split(',');
+            } else if (_.contains(elem.split(''), ' ')) {
+                numbers = elem.split(' ');
+            } else {
+                numbers = elem.split('');
+            }
+
+            _.each(numbers, (el, ind) => {
+                numbers[ind] = +numbers[ind]; //convert to integers
+            });
+
+            console.log('numbers ' , numbers, '\nArray.isArray(numbers)' , Array.isArray(numbers));
+
+            var result = arraySum(numbers);
+            console.log('result ' , result);
+            var ans = $('#sum-result').append(result).hide();
+            ans.slideDown();
+        });
+
+        $('#sum-clear').click(function (event) {
+            event.preventDefault();
+            location.reload(); //clear both fields          
+        });
+
+        //No. 5 - Write a JavaScript program to compute the exponent of a number.
+        var exponent = (base, exp) => {
+            var result = null;
+
+            if (exp === 1) {
+                result = Math.pow(base, exp);
+            } else {
+                result = base * exponent(base, exp - 1);
+            }
+            return result;
+        };
+
+        console.log(exponent(4, 3));
+
+        $('#exp-result').hide();
+
+        $('#exp-button').click(function (e) {
+            e.preventDefault();
+            var base = $('#exp-field-base').val();            
+            var exp = $('#exp-field-exp').val();            
+            var result = commaSeparateNumber(exponent(base, exp));
+            console.log('result ' , result);
+            var ans = $('#exp-result').append(result).hide();
+            ans.slideDown();
+        });
+
+        $('#exp-clear').click(function (event) {
+            event.preventDefault();
+            location.reload(); //clear both fields          
+        });
+
+        //No. 6 - Write a JavaScript program to get the first n Fibonacci numbers.
+        var fibonacci = (num) => {
+            var result = null
+            var resultArr = [];
+            var ind = 0;
+
+            //calculate fibonacci number at 'n'
+            var fibo = (n) => (n < 1) ? 0 : (n < 2) ? 1 : fibo(n - 2) + fibo(n - 1);
+            
+
+            while (ind <= num) {
+                resultArr.push(fibo(ind));
+                ind++;                
+            }            
+            result = resultArr.join(', ');            
+            return result;            
+        };            
+
+        console.log(fibonacci(13));
+
+        $('#fibo-result').hide();
+
+        $('#fibo-button').click(function (e) {
+            e.preventDefault();
+            var num = $('#fibo-field').val();                        
+            var result = commaSeparateNumber(fibonacci(num));
+            console.log('result ' , result);
+            var ans = $('#fibo-result').append(result).hide();
+            ans.slideDown();
+        });
+
+        $('#fibo-clear').click(function (event) {
+            event.preventDefault();
+            location.reload(); //clear both fields          
+        });
+
+        //No. 7 - Write a JavaScript program to check whether a number is even or not.
+        var evenOrNot = (num) => {            
+            var result = null;
+            var lastDigit = parseInt(num.toString().split('').pop());
+            console.log('lastDigit ' , lastDigit);;
+
+            if (lastDigit === 2) {
+                result = 'even';
+            } else if (lastDigit === 3) {
+                result = 'odd';
+            } else {                
+                lastDigit -= 2;
+                result = evenOrNot(lastDigit);
+            }
+            return result;
+        };
+
+        console.log(evenOrNot(13));
+        console.log(evenOrNot(18));
+
+        $('#even-result').hide();
+
+        $('#even-button').click(function (e) {
+            e.preventDefault();
+            var num = $('#even-field').val();                        
+            var result = evenOrNot(num);
+            console.log('result ' , result);
+            var ans = $('#even-result').append(result).hide();
+            ans.slideDown();
+        });
+
+        $('#even-clear').click(function (event) {
+            event.preventDefault();
+            location.reload(); //clear both fields          
+        });
+
+        //No. 8 - Write a JavaScript program for binary search.        
+        var binarySearch = (array, target) => {            
+            var result = null;
+            var lo = 0;
+            console.log('lo ' , lo);
+            var hi = array.length - 1;
+            console.log('hi ' , hi);
+
+            //sort
+            array.sort((a,b) => a - b);
+            console.log('array ' , array);
+
+            var _binarySearch = (modArray, hi, lo, target) => {                
+                //median
+                var median = lo + Math.round((hi - lo) / 2);
+                console.log('median (OUTSIDE)' , median);
+
+                console.log('modArray[median] ' , modArray[median], '\nmedian ' , median, '\ntarget ' , target);
+
+                if ((modArray[median] - target) === 0) {                                        
+                    result = median;                                    
+                    console.log('result ' , result);
+                } else {                    
+                    if (modArray[median] < target) {                        
+                        lo = median;                        
+                        result = _binarySearch(modArray, hi, lo, target);                                                
+                    } else if (modArray[median] > target) {
+                        hi = median;
+                        result = _binarySearch(modArray, hi, lo, target);
+                    }
+                }
+                return result;                
+            };
+            _binarySearch(array, hi, lo, target);
+            return result;            
+        };
+
+        //console.log(binarySearch([0,19,68,13,5,22,41,55,81,72,98], 55));
+
+        $('#binary-result').hide();
+
+        $('#binary-button').click(function (e) {
+            e.preventDefault();
+
+            var elem = $('#binary-field').val();
+            console.log('elem ' , elem, 'typeof(elem)' , typeof(elem));
+
+            //create array of values
+            var numArr = [];
+            if (_.contains(elem.split(''), ',')) {
+                numArr = elem.split(',');
+            } else if (_.contains(elem.split(''), ' ')) {
+                numArr = elem.split(' ');
+            } else {
+                numArr = elem.split('');
+            }
+
+            _.each(numArr, (el, ind) => {
+                numArr[ind] = +numArr[ind]; //convert to integers                
+            });
+
+            console.log('numArr ' , numArr);
+
+            var target = $('#binary-field-target').val();
+            console.log('target ' , target);            
+            var result = binarySearch(numArr, target);
+            console.log('result ' , result);
+            var ans = $('#binary-result').append(result).hide();
+            ans.slideDown();
+        });
+
+        $('#binary-clear').click(function (event) {
+            event.preventDefault();
+            location.reload(); //clear both fields          
+        });
+
+        //Flatten
+        var flatten = (nestedArray, result) => {
+          var result = [];
+            var _flatten = (array) => {
+                _.each(array, (el) => {
+                    if (Array.isArray(el)) {
+                        _flatten(el);
+                    } else {
+                        result.push(el);
+                    }            
+                });          
+            };
+        _flatten(nestedArray);
+        return result;
+        };        
+
+        console.log(flatten([1, [2], [3, [[4]]]]));
+
+        //Factorial Array
+        var factorialArray = function(n) {
+            //Your code here
+            var factArr = [];
+            var result = null;
+            var i = 0;
+
+            function _factorialArray (num) {
+                if (num < 0 || isNaN(num)) {
+                    result = undefined;
+                } else if (num === 0) {
+                    result = 1;
+                } else {
+                    result = num * _factorialArray(num - 1);
+                }
+                return result;
+            };
+
+            if (n >= 0) {
+                while (i <= n) {        
+                    _factorialArray(i);
+                    factArr.push(result);
+                    i++;
+                }
+            } else {
+                factArr = undefined;
+            }
+            
+
+            return factArr;
+        };
+
+        console.log(factorialArray(12));
+
+        $('#factorial-array-result').hide();
+
+        $('#factorial-array-button').click(function (e) {
+            e.preventDefault();
+            var myVal = $('#factorial-array-field').val();
+            var result = factorialArray(myVal);
+            console.log('result ' , result);
+            var ans = $('#factorial-array-result').append('[ ' + result + ' ]').hide();
+            ans.slideDown();
+        });
+
+        $('#factorial-array-clear').click(function (event) {
+            event.preventDefault();
+            location.reload(); //clear both fields          
+        });
